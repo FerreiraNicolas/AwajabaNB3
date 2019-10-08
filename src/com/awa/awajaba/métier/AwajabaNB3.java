e79c2990c82dce0de11ef0533d108e8854f12476
Nicolas <etudiant@debian9-gui.sio.lla.fr>
2019-10-08 16:24:47 +0200

    Ajout du constructeur(Et modification du package

---
+++ src/com/awa/awajaba/métier/AwajabaNB3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.awa.awajaba.métier;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author etudiant
+ */
+
+
+class Repas{
+	
+	private int numero;
+	private LocalDate date;
+	
+        
+        	Repas(int numero, LocalDate date){
+		this.numero= numero;
+		this.date= date;
+	}
+
+    
+}
